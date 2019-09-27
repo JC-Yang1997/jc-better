beb3c30a0bfe4454b629a1445a80422394d9c9ea
yangjinchun <yjc@tuotuonet.com>
2019-09-27 16:09:06 +0800

    init:初始化wepy项目

---
+++ src/app.vue
@@ -0,0 +1,25 @@
+<script>
+import wepy from 'wepy'
+
+export default class extends wepy.app {
+  config = {
+    pages: [
+      'pages/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'js-better',
+      navigationBarTextStyle: 'black'
+    }
+  }
+
+  globalData = {}
+
+  constructor () {
+    super()
+  }
+
+  onLaunch() {}
+}
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <view>
+    index page
+  </view>
+</template>
+
+<script>
+import wepy from 'wepy'
+
+export default class Index extends wepy.page {
+  config = {
+    navigationBarTitleText: 'index page'
+  }
+  components = {}
+
+  data = {
+    
+  }
+
+  computed = {
+  }
+
+  methods = {
+  }
+
+  onLoad() {
+    console.log('index page load')
+  }
+}
+</script>
